f24a9fdafb64d3e6f473f6b882d1038bc97d1a26
Marco Teti <sgttenore.gmail.com>
2019-05-22 20:06:19 -0700

    added body-parser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
@@ -9,8 +10,8 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, './app/public')));
 
